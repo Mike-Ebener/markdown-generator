c51fbde6a79c5971a6a9d4a5e739726bd18810dd
ME <mebener44@gmail.com>
2021-05-18 22:59:32 -0700

    setup file structure

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var message = 'Hello Node!';
+
+var sum = 5 + 3;
+
+console.log(message);
+console.log(sum);
+
+
+/*// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+*/
